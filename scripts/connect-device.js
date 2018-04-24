37811a87d107aa9b5fe0df807a1e31d940096f1f
Magne Rodem <magne@rodem.no>
2018-04-24 08:59:44 +0200

    Allow entering AWS IoT endpoint when connecting

--- scripts/connect-device.js
+++ scripts/connect-device.js
@@ -1,36 +1,43 @@
 const path = require('path');
 const awsIot = require('aws-iot-device-sdk');
+const inquirer = require('inquirer');
 
 const certDir = path.resolve(__dirname, '../cert');
 
-const device = awsIot.device({
-    keyPath: `${certDir}/deviceCert.key`,
-    certPath: `${certDir}/deviceCertAndCACert.crt`,
-    caPath: `${certDir}/aws-iot-root.cert`,
-    clientId: '12345678',
-    host: 'a31tdzaj9dwy1x.iot.us-east-1.amazonaws.com',
-});
+inquirer.prompt([{
+    type: 'input',
+    name: 'host',
+    message: 'Enter the AWS IoT endpoint to connect to (<id>.iot.<region>.amazonaws.com):',
+}]).then(({ host }) => {
+    const device = awsIot.device({
+        keyPath: `${certDir}/deviceCert.key`,
+        certPath: `${certDir}/deviceCertAndCACert.crt`,
+        caPath: `${certDir}/aws-iot-root.cert`,
+        clientId: '12345678',
+        host,
+    });
 
-device.on('connect', () => {
-    console.log('connected');
-});
+    device.on('connect', () => {
+        console.log('connected');
+    });
 
-device.on('close', () => {
-    console.log('close');
-});
+    device.on('close', () => {
+        console.log('close');
+    });
 
-device.on('reconnect', () => {
-    console.log('reconnect');
-});
+    device.on('reconnect', () => {
+        console.log('reconnect');
+    });
 
-device.on('offline', () => {
-    console.log('offline');
-});
+    device.on('offline', () => {
+        console.log('offline');
+    });
 
-device.on('error', function(error) {
-    console.log('error', error);
-});
+    device.on('error', function(error) {
+        console.log('error', error);
+    });
 
-device.on('message', function(topic, payload) {
-    console.log('message', topic, payload.toString());
+    device.on('message', function(topic, payload) {
+        console.log('message', topic, payload.toString());
+    });
 });
